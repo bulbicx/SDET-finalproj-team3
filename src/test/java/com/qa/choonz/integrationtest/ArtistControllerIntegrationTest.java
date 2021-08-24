e75f659dff5ea6c82ffcdde115dd0e02e66fa91d
Marco Castellana <arkanoidmark96@gmail.com>
2021-08-24 11:08:55 +0100

    MC: added sql files

---
+++ src/test/java/com/qa/choonz/integrationtest/ArtistControllerIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.qa.choonz.integrationtest;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:sql-schema.sql", "classpath:sql-data.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public class ArtistControllerIntegrationTest {
+
+}
