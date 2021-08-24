2c2e4944b63c122e1c8f1afe7587c5135f0a1b69
Leaf-Cooper-QA <85402946+Leaf-Cooper@users.noreply.github.com>
2021-08-24 14:36:12 +0100

    added unit tests for domain, dto and services

--- src/main/java/com/qa/choonz/persistence/domain/User.java
+++ src/main/java/com/qa/choonz/persistence/domain/User.java
@@ -101,7 +101,7 @@ public class User {
 	public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("User [id=").append(id).append(", username=").append(username).append(", name=")
-        		.append(name).append(", playlists=").append(playlists);
+        		.append(name).append(", playlists=").append(playlists).append("]");
         return builder.toString();
 	}
 
