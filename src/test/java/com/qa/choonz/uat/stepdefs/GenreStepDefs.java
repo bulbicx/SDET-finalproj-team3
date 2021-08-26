3503958e8fb519c28ecb632fdce325f980fe1e59
dindiongco <dindiongco@qa.com>
2021-08-26 16:43:44 +0100

    Added modified genre user acceptance.

--- src/test/java/com/qa/choonz/uat/stepdefs/GenreStepDefs.java
+++ src/test/java/com/qa/choonz/uat/stepdefs/GenreStepDefs.java
@@ -26,42 +26,42 @@ public class GenreStepDefs {
 	}
 	
 	@Given("I am on the genres page")
-	public void i_am_on_the_genres_page() {
+	public void iAmOnTheGenresPage() {
 	    this.driver.get(genresPage.url);
 	}
 
 	@When("I click on the first genre")
-	public void i_click_on_the_first_genre() {
+	public void iClickOnTheFirstGenre() {
 	    genresPage.clickCard();
 	}
 
 	@Then("I am taken to the page for that genre")
-	public void i_am_taken_to_the_page_for_that_genre() {
+	public void iAmTakenToThePageForThatGenre() {
 	    assertEquals("http://127.0.0.1:5500/genresingle.html?id=1", this.driver.getCurrentUrl());
 	}
 
 	@Given("I am on the genre single page")
-	public void i_am_on_the_genre_single_page() {
+	public void iAmOnTheGenreSinglePage() {
 	    this.driver.get(genreSinglePage.url);
 	}
 
 	@When("I click the name of the first album")
-	public void i_click_the_name_of_the_first_album() {
+	public void iClickTheNameOfTheFirstAlbum() {
 	    genreSinglePage.clickAlbumName();
 	}
 
 	@Then("I am taken to the page of that album")
-	public void i_am_taken_to_the_page_of_that_album() {
+	public void iAmTakenToThePageOfThatAlbum() {
 	    assertEquals("http://127.0.0.1:5500/albumsingle.html?id=1", this.driver.getCurrentUrl());
 	}
 
 	@When("I click on the name of the artist")
-	public void i_click_on_the_name_of_the_artist() {
+	public void iClickOnTheNameOfTheArtist() {
 	    genreSinglePage.clickArtistName();
 	}
 
 	@Then("I am taken to the page of that artist")
-	public void i_am_taken_to_the_page_of_that_artist() {
+	public void iAmTakenToThePageOfThatArtist() {
 	    assertEquals("http://127.0.0.1:5500/artistsingle.html?id=1", this.driver.getCurrentUrl());
 	}
 
