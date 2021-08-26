90e72e2899825aae640a43516c1098431521f5f4
Marco Castellana <arkanoidmark96@gmail.com>
2021-08-26 12:09:22 +0100

    MC: footer element and style created

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -3,34 +3,35 @@
   let navbar =
     `
     <nav class="navbar fixed-top navbar-expand-lg bg-light navbar-light">
-      <a class="navbar-brand" href="./">
-      <img class="logo" src="../img/Choonz.jpg" width="auto" ml-auto height="50"></a>
+      <a class="navbar-brand mx-3" href="./">
+        <img class="logo p-0" src="../img/Choonz.jpg" width="auto" height="50">
+      </a>
       <button class="navbar-toggler collapsed border-0" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
         <span class="navbar-toggler-icon"></span>
         <div class="close-icon py-1">✖</div>
       </button>
       <div class="collapse navbar-collapse" id="collapsingNavbar">
-        <ul class="nav navbar-nav">
+        <ul class="nav navbar-nav text-center">
           <li class="nav-item">
-            <a class="nav-link" href="/index.html">Home</a>
+            <a class="nav-link" href="index.html">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/artists.html">Artists</a>
+            <a class="nav-link" href="artists.html">Artists</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="/albums.html">Albums</a>
+              <a class="nav-link" href="albums.html">Albums</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="/playlists.html">Playlists</a>
+              <a class="nav-link" href="playlists.html">Playlists</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/genres.html">Genres</a>
+            <a class="nav-link" href="genres.html">Genres</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/playlists.html">Profile</a>
+            <a class="nav-link" href="#">Profile</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/playlists.html">Login</a>
+            <a class="nav-link" href="#">Login</a>
           </li>
         </ul>
       </div>
