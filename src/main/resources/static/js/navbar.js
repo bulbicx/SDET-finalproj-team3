c7256d225ba561a6b93eeac42982f34de0817d27
Marco Castellana <arkanoidmark96@gmail.com>
2021-08-26 13:28:09 +0100

    MC: added style to active page nav link

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,8 +1,7 @@
 (() => {
   let containerNav = document.querySelector("#nav-container");
-  let navbar =
-    `
-    <nav class="navbar fixed-top navbar-expand-lg bg-light navbar-light">
+  let navbar = `
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light">
       <a class="navbar-brand mx-3" href="./">
         <img class="logo p-0" src="../img/Choonz.jpg" width="auto" height="50">
       </a>
@@ -16,19 +15,19 @@
             <a class="nav-link" href="index.html">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="artists.html">Artists</a>
+            <a class="nav-link nav-artist" href="artists.html">Artists</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="albums.html">Albums</a>
+              <a class="nav-link nav-album" href="albums.html">Albums</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="playlists.html">Playlists</a>
+              <a class="nav-link nav-playlist" href="playlists.html">Playlists</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="genres.html">Genres</a>
+            <a class="nav-link nav-genre" href="genres.html">Genres</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Profile</a>
+            <a class="nav-link nav-profile" href="#">Profile</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Login</a>
@@ -38,6 +37,25 @@
     </nav>
     `;
   
-  containerNav.innerHTML = navbar;
+    
+    console.log(window.location.href)
+    containerNav.innerHTML = navbar;
+    
+  let navArtist = document.querySelector(".nav-artist");
+  let navAlbum = document.querySelector(".nav-album");
+  let navPlaylist = document.querySelector(".nav-playlist");
+  let navGenre = document.querySelector(".nav-genre");
+  let navProfile = document.querySelector(".nav-profile");
 
+    if (window.location.href.includes("artists")) {
+      navArtist.setAttribute("class", "nav-link nav-artist nav-link-active");
+    } else if (window.location.href.includes("albums")) {
+      navAlbum.setAttribute("class", "nav-link nav-album nav-link-active");
+    } else if (window.location.href.includes("playlists")) {
+      navPlaylist.setAttribute("class", "nav-link nav-playlist nav-link-active");
+    } else if (window.location.href.includes("genres")) {
+      navGenre.setAttribute("class", "nav-link nav-genre nav-link-active");
+    } else if (window.location.href.includes("profile")) {
+      navProfile.setAttribute("class", "nav-link nav-profile nav-link-active");
+    }
 })();
