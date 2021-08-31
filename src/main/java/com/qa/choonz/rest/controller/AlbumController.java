69aaacd2c64cad6e60800418a4c906df6ae8434f
dindiongco <dindiongco@qa.com>
2021-08-31 15:03:34 +0100

    Added small changes.

--- src/main/java/com/qa/choonz/rest/controller/AlbumController.java
+++ src/main/java/com/qa/choonz/rest/controller/AlbumController.java
@@ -42,7 +42,7 @@ public class AlbumController {
     }
 
     @GetMapping("/read/{id}")
-    public ResponseEntity<AlbumDTO> read(@PathVariable long id) {
+    public ResponseEntity<AlbumDTO> read(@PathVariable Long id) {
         return new ResponseEntity<AlbumDTO>(this.service.read(id), HttpStatus.OK);
     }
     
@@ -57,13 +57,13 @@ public class AlbumController {
 	}
 
     @PutMapping("/update/{id}/{artistId}/{genreId}")
-    public ResponseEntity<AlbumDTO> update(@RequestBody Album album, @PathVariable long id, @PathVariable (value="artistId") Long artistId,
+    public ResponseEntity<AlbumDTO> update(@RequestBody Album album, @PathVariable Long id, @PathVariable (value="artistId") Long artistId,
     		@PathVariable (value="genreId") Long genreId) {
         return new ResponseEntity<AlbumDTO>(this.service.update(album, id, artistId, genreId), HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<AlbumDTO> delete(@PathVariable long id) {
+    public ResponseEntity<AlbumDTO> delete(@PathVariable Long id) {
         return this.service.delete(id) ? new ResponseEntity<AlbumDTO>(HttpStatus.NO_CONTENT)
                 : new ResponseEntity<AlbumDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
