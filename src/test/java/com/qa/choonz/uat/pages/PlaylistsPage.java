1e0399acc11c3a7952acaa180d9e7e8a24dcbeef
Marco Castellana <arkanoidmark96@gmail.com>
2021-09-01 11:40:11 +0100

    MC: added create playlist uat

--- src/test/java/com/qa/choonz/uat/pages/PlaylistsPage.java
+++ src/test/java/com/qa/choonz/uat/pages/PlaylistsPage.java
@@ -16,6 +16,21 @@ public class PlaylistsPage {
 	@FindBy(xpath="/html/body/section/main/div")
 	public WebElement firstCard;
 	
+	@FindBy(id = "new-name")
+	private WebElement newName;
+	
+	@FindBy(id = "new-description")
+	private WebElement newDescription;
+	
+	@FindBy(id = "new-artwork")
+	private WebElement newArtwork;
+	
+	@FindBy(id = "new-user")
+	private WebElement newUser;
+	
+	@FindBy(className = "add")
+	private WebElement addBtn;
+	
 	public void clickCard() {
 		firstCard.click();
 	}
@@ -27,5 +42,24 @@ public class PlaylistsPage {
 	public void clickEditIcon() {
 		this.editIcon.click();
 	}
+	
+	public void insertData(String name, String description, String artwork, String user) {
+		this.newName.sendKeys(name);
+		this.newDescription.sendKeys(description);
+		this.newArtwork.sendKeys(artwork);
+		this.newUser.sendKeys(user);
+	}
+	
+	public void clickAddBtn() {
+		this.addBtn.click();
+	}
 
+	public void addPlaylist(String name, String description, String artwork, String user) {
+		this.plusIcon.click();
+		this.newName.sendKeys(name);
+		this.newDescription.sendKeys(description);
+		this.newArtwork.sendKeys(artwork);
+		this.newUser.sendKeys(user);
+		this.addBtn.click();
+	}
 }
--- src/test/java/com/qa/choonz/uat/stepdefs/PlaylistCrudStepDefs.java
+++ src/test/java/com/qa/choonz/uat/stepdefs/PlaylistCrudStepDefs.java
@@ -1,8 +1,16 @@
 package com.qa.choonz.uat.stepdefs;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.choonz.uat.hooks.SeleniumHooks;
+import com.qa.choonz.uat.pages.HomePage;
+import com.qa.choonz.uat.pages.PlaylistsPage;
+import com.qa.choonz.uat.pages.SignUpPage;
 import com.qa.choonz.utils.ScreenshotUtility;
 
 import io.cucumber.java.AfterStep;
@@ -15,47 +23,61 @@ public class PlaylistCrudStepDefs {
 	
 	private WebDriver driver;
 	private ScreenshotUtility screenshotUtils;
-	private PlaylistPage playlistPage;
+	private PlaylistsPage playlistPage;
+	private SignUpPage signupPage;
+	private HomePage homePage;
 	
 	public PlaylistCrudStepDefs(SeleniumHooks hooks) {
 		this.driver = hooks.getDriver();
 		screenshotUtils = new ScreenshotUtility();
+		this.playlistPage = PageFactory.initElements(driver, PlaylistsPage.class);
+		this.signupPage = PageFactory.initElements(driver, SignUpPage.class);
+		this.homePage = PageFactory.initElements(driver, HomePage.class);
 	}
 
 	@Given("I have an existing user")
 	public void i_have_an_existing_user() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    this.driver.get(signupPage.URL);
+	    signupPage.clickSignUpLink();
+	    String name = "Charlie";
+	    String username = "charlie90";
+	    String password = "password123";
+	    signupPage.createUser(name, username, password);
 	}
 
 	@When("I am in the playlist page")
 	public void i_am_in_the_playlist_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    homePage.clickHeaderPlaylistsBtn();
+	    assertTrue(this.driver.getTitle().equals("Playlists"));
 	}
 
 	@When("I go to add a new playlist")
 	public void i_go_to_add_a_new_playlist() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    playlistPage.clickPlusIcon();
 	}
 
 	@When("I add playlist details")
-	public void i_add_playlist_details() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void i_add_playlist_details() throws InterruptedException {
+		String name = "Playlist name";
+		String description = "Some description";
+		String artwork = "artwork image";
+		String user = "1";
+	    playlistPage.insertData(name, description, artwork, user);
+	    Thread.sleep(2000);
 	}
 
 	@When("I press the add playlist button")
-	public void i_press_the_add_playlist_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void i_press_the_add_playlist_button() throws InterruptedException {
+		playlistPage.clickAddBtn();
+	    Thread.sleep(2000);
 	}
 
 	@Then("a new playlist is added")
 	public void a_new_playlist_is_added() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    WebDriverWait wait = new WebDriverWait(driver, 5);
+	    wait.until(ExpectedConditions.alertIsPresent());
+		String alertMsg = this.driver.switchTo().alert().getText();
+		assertTrue(alertMsg.equals("Playlist added!"));
 	}
 	
 	@AfterStep
