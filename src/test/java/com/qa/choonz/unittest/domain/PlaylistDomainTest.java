8bb3e7349b8d2507a5147150af40f23f62ffe4cd
Marco Castellana <arkanoidmark96@gmail.com>
2021-09-02 14:38:39 +0100

    MC: fixed album image for user

--- src/test/java/com/qa/choonz/unittest/domain/PlaylistDomainTest.java
+++ src/test/java/com/qa/choonz/unittest/domain/PlaylistDomainTest.java
@@ -12,14 +12,11 @@ import com.qa.choonz.persistence.domain.PublicUser;
 
 public class PlaylistDomainTest {
 	
-<<<<<<< HEAD
 	private PublicUser user = new PublicUser(0L, "username", "real name", "password", new ArrayList<>());
 	private Playlist playlist = new Playlist(0L, "playlist name", "playlist desc", "artwork", new ArrayList<>(), user);
-=======
 	private Image image = new Image(0L, "image name", "image type", null);
 	private User user = new User(0L, "username", "real name", "password", new ArrayList<>());
 	private Playlist playlist = new Playlist(0L, "playlist name", "playlist desc", image, new ArrayList<>(), user);
->>>>>>> 5a52af1a816c2c70d032b636638d139a1e5d0305
 	
 	@Test
 	public void PlaylistTest() {
